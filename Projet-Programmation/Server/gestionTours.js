b9f358b58cb017296b65648d71eb8a2155533ec1
Le_redstonneur <50110370+LeRedstonneur@users.noreply.github.com>
2023-12-14 00:44:33 +0100

    Reformatage du code

--- Projet-Programmation/Server/gestionTours.js
+++ Projet-Programmation/Server/gestionTours.js
@@ -1,52 +1,62 @@
 var tour = 0;
 
-var gestionTours = function(io, db, partie, finPartie) {
-    //setInterval(()=>{
-    //while (tour >= 0){
-        // On récupère la liste des joueurs qui peuvent encore jouer
-        try {
-            const recupererMains = 'SELECT pseudo, main FROM joueurs, joue WHERE idPartie = ? AND joue.idJ=joueurs.idJ';
-            db.query(recupererMains, [partie.idPartie], async (err, result) => {
-                if (err) {
-                    console.log('Erreur lors de la connexion');
-                    throw err;
-                } 
-                if(result.length > 10){
-                    result.forEach(element => {
-                        console.log("\n" + element.pseudo + "\n" + element.main + "\n");
-                    });
-                } else {
-                    tour = -2
-                    // On attend que tous les joueurs aient quitté la partie
-                    const compterJoueurs = setInterval(() => {
-                        db.query("SELECT * FROM joue WHERE idPartie = ?", [partie.idPartie], async (err, result) => {
-                            if(err){
-                                throw err;
-                            }
-                            nbJoueurs = new Map(Array.from(result, (key, value) => [key, value])).size;
-                            console.log("Il y a " + nbJoueurs + " joueurs dans la partie " + partie.idPartie);
-                            if (nbJoueurs == 0){
-                                // Plus aucun joueur n'est dans la partie
-                                clearInterval(compterJoueurs);
-                                console.log("Il n'y a plus aucun joueur");
-                                finPartie()
-                                db.query("DELETE FROM parties WHERE idPartie = ?", [partie.idPartie], async(err, result) =>{
-                                    if(err || result.affectedRows != 1){
-                                        console.log("La partie a déjà été supprimée");
-                                    } else {
-                                        console.log("Normalement la partie a bien été supprimée");
-                                    }
-                                });
-                            }
-                        });
-                    },1000);        
-                }
-            });
-        } catch (error) {
-            console.error("Erreur : " + error);
+var gestionTours = function(io, db, partie) {
+    io.on('playerLeaving', () => { // Quand un joueur quitte la partie
+        db.query("SELECT * FROM joue WHERE idPartie = ?", [partie.idPartie], async (err, result) => {
+            if(err){
+                throw err;
+            }
+            nbJoueurs = new Map(Array.from(result, (key, value) => [key, value])).size;
+            console.log("Il y a " + nbJoueurs + " joueurs dans la partie " + partie.idPartie);
+            if (nbJoueurs == 0){
+                // Plus aucun joueur n'est dans la partie
+                clearInterval(compterJoueurs);
+                io.emit
+                console.log("Il n'y a plus aucun joueur");
+                db.query("DELETE FROM parties WHERE idPartie = ?", [partie.idPartie], async(err, result) =>{
+                    if(err || result.affectedRows != 1){
+                        console.log("La partie a déjà été supprimée");
+                    } else {
+                        console.log("Normalement la partie a bien été supprimée");
+                    }
+                });
+            }
+        });
+    });
+    mainsBis = null;
+    console.log(mainsBis);
+    recupererMains(db, partie.idPartie,mainsBis);
+    while(mainsBis == null){
+        // console.log(mainsBis);
+    }
+    console.log(mainsBis);
+}
+
+function recupererMains(db,idPartie, retour){
+    retour = null;
+    db.query('SELECT pseudo, main FROM joueurs, joue WHERE idPartie = ? AND joue.idJ=joueurs.idJ', [idPartie], async(err, result) =>{
+        console.log("ici");
+        if(err){
+            console.log('Erreur lors de la connexion');
+            throw err;
         }
-    //}
-    //,5000);
+        var mains = new Map();
+        result.forEach(element => {
+            mainDuJoueur = JSON.parse(element.main);
+            if(mainDuJoueur.length > 0){
+                console.log("Le joueur " + element.pseudo + " a " + mainDuJoueur.length + " carte(s)")
+                mains.set(element.pseudo, mainDuJoueur);
+            } else {
+                console.log("Le joueur " + element.pseudo + " n'a plus de cartes");
+            }
+        });
+        // console.log(mains);
+        console.log("Il y a " + mains.size + " joueurs qui ont encore des cartes");
+        for(const [key, value] of mains){
+            retour.set(key,value);
+        }
+        console.log("Affectation mais c'est trop tard ?")
+    });
 }
 
 module.exports = gestionTours;
