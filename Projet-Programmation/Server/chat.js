24e6907384285f19adf66f777f805ce58b7c225d
Xerondo <72127675+Xerondo@users.noreply.github.com>
2023-12-14 22:24:27 +0100

    Add files via upload

--- Projet-Programmation/Server/chat.js
+++ Projet-Programmation/Server/chat.js
@@ -1,23 +1,18 @@
 var chatPartie = function(io, socket, db) {
     socket.on('chat', (data) => { // data doit contenir le pseudonyme (pas son idJoueur), son message et l'idPartie
         console.log("le joueur", data.joueur, "veut envoyer ce message :", data.message);
-        db.query("SELECT idPartie FROM joue, joueur WHERE joue.idJ = joueur.idJ AND idJ = ? AND idPartie = ?", [data.joueur, data.partie], async(err, result) => {
+        db.query("SELECT idPartie FROM joue, joueur WHERE joue.idJ = joueur.idJ AND idJ = ? AND idPartie = ?", [data.joueur, data.party], async(err, result) => {
             if(err){
                 throw err;
             }
-            if(result.lenght == 0){ // Le joueur est dans aucune partie
+            if(!result){ // Le joueur est dans aucune partie
                 console.log("Le joueur veut envoyer un message sans être dans une partie");
             } else {
-                console.log("le joueur est bien présent dans la partie :", data.partie);
-                io.in(data.partie).emit('newMessage', { joueur : data.joueur, message : data.message });
-                console.log("le message va être envoyé dans la partie", data.partie);
-            
+                console.log("le joueur est bien présent dans la partie :", data.party);
+                io.in(data.party).emit('newMessage', { joueur : data.joueur, message : data.message });
+                console.log("le message va être envoyé dans la partie", data.party);
             }
         })
-    })
-
-    socket.on('chatAdmin', (data) => { // data doit contenir le message à envoyer et l'idPartie
-
     })
 }
 
