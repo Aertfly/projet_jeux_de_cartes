220b05c0613f8953cc7c0bb9e5b33918873a2d3d
Xerondo <72127675+Xerondo@users.noreply.github.com>
2023-12-15 10:52:07 +0100

    Add files via upload

---
+++ Projet-Programmation/Server/scores.js
@@ -0,0 +1,15 @@
+var scores = function(io, socket, db) {
+    socket.on('updateScores', (data) => { // data doit contenir l'idJ, son nouveau score, et son idPartie
+        console.log("le joueur", data.player, "a augmenté son score, qui est désormais à", data.score, "(dans la partie", data.party, ").");
+        db.query('UPDATE joue SET score = ? WHERE idJ = ? AND idPartie = ?', [data.score, data.player, data.party], async(err, result) => {
+            if(err){
+                console.log("la modification du score a échoué pour le joueur", data.player, "dans la partie", data.party);
+                throw err;
+            }
+            io.to(data.party).emit('updatedScore', {player : data.player, newScore : data.score, party : data.party});
+
+        })
+    })
+}
+
+module.exports = scores;
