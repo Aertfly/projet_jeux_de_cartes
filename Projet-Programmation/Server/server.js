c281e98b1406edd9065a49f55ecb531a2a756e0b
Xerondo <72127675+Xerondo@users.noreply.github.com>
2023-12-15 08:28:43 +0100

    modif légère pour pouvoir traiter les reconnexions

--- Projet-Programmation/Server/server.js
+++ Projet-Programmation/Server/server.js
@@ -121,9 +121,14 @@ io.on('connection', (socket) => {
     });
 
 
-    socket.on('disconnect', (reason) => {
-        console.log('Un utilisateur s\'est déconnecté ' + reason + " " + socket.id);
-        delete connectedUsers[socket.id];
+    socket.on('disconnect', (reason) => { // changement pour l'abandon involontaire (Killian)
+        if (reason === 'ping timeout') {
+            // Si le joueur se reconnecte après une déconnexion par manque de co
+            socket.emit('playerReconnect', {player : socket.id, party : socket.idPartie});
+        } else {
+            console.log('Un utilisateur s\'est déconnecté ' + reason + " " + socket.id);
+            delete connectedUsers[socket.id];
+        }
     });
 
   //socket importé des autres fichiers
