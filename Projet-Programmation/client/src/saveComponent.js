40aa31a3ea5f323cc4ea81c77f6a78bc28c2fad9
Xerondo <72127675+Xerondo@users.noreply.github.com>
2023-12-16 21:49:26 +0100

    Add files via upload

--- Projet-Programmation/client/src/saveComponent.js
+++ Projet-Programmation/client/src/saveComponent.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client';
 const socket = io('http://localhost:3001');
 // pour accéder à ce composant, mettre "import Save from './saveComponent.js';"
 
-function Save({ idPartie }) {
+function Save({ data }) {
+    // Enregistrement de la valeur de la data
+    const [party, setParty] = useState('');
+    setParty(data.party);
+
   const handleSave = () => {
-    console.log("La partie", idPartie, "demande à être mise en pause");
-    socket.emit('saveParty', { partie: idPartie });
+    console.log("La partie", party, "demande à être mise en pause");
+    socket.emit('saveParty', { partie: party });
   };
 
   return (
