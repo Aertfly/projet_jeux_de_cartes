cc81bee3e00f444f573cbaa0b5f9bb66f5f833b9
Xerondo <72127675+Xerondo@users.noreply.github.com>
2023-12-18 01:33:39 +0100

    Update chat.js (killian)

--- Projet-Programmation/Server/chat.js
+++ Projet-Programmation/Server/chat.js
@@ -1,11 +1,10 @@
 var chatPartie = function(io, socket, db) {
     socket.on('chat', (data) => { // data doit contenir le pseudonyme (pas son idJoueur), son message et l'idPartie
         console.log("le joueur", data.username, "veut envoyer ce message :", data.message);
-        console.log("ce joueur est bien présent dans la partie :", data.party);
         io.in(data.party).emit('newMessage', { username : data.username, message : data.message });
-        console.log("le message va être envoyé dans la partie", data.party);
+        console.log("ce message va être envoyé dans la partie", data.party); 
             
     })
 }
 
-module.exports = chatPartie;
+module.exports = chatPartie; 
