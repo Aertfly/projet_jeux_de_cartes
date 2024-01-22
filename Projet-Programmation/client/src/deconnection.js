40217d502f6d2e9425aadc367a871697e8d95408
Fraizard <94200448+Fraizard@users.noreply.github.com>
2024-01-22 16:23:42 +0100

    Update deconnection.js

--- Projet-Programmation/client/src/deconnection.js
+++ Projet-Programmation/client/src/deconnection.js
@@ -15,12 +15,10 @@ function Deco(props) {
     setEstConnecte("Déconnecté");
     socket.emit('deconnexion');
     navigate('/');
-    socket.emit('playerLeaving',idJ);
   };
 
   window.onload = function(e) {
     e.preventDefault();
-    handleDeconnection();
     e.returnValue = 'Déconnecté'; 
   };
 
