7134773a898a13fa876ff2aadeab67c1c17641d5
Aertfly <jean.colib.mfc@gmail.com>
2024-01-23 11:12:20 +0100

    update

--- Projet-Programmation/client/src/Page/Component/saveComponent.js
+++ Projet-Programmation/client/src/Page/Component/saveComponent.js
@@ -5,12 +5,11 @@ const socket = io('http://localhost:3001');
 
 function Save({ data }) {
     // Enregistrement de la valeur de la data
-    const [party, setParty] = useState('');
-    setParty(data.party);
+    const [party, setParty] = useState(data.party);
 
   const handleSave = () => {
     console.log("La partie", party, "demande à être mise en pause");
-    socket.emit('saveParty', { partie: party });
+    socket.emit('saveParty', { 'partie': party });
   };
 
   return (
