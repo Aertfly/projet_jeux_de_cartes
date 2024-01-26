79e498d02161918e1f3ff95f67c012e6ad38be98
Xerondo <72127675+Xerondo@users.noreply.github.com>
2024-01-26 10:07:20 +0100

    Mettre le bouton juste à droite du quitter (plus propre)

--- Projet-Programmation/client/src/Page/Component/saveComponent.js
+++ Projet-Programmation/client/src/Page/Component/saveComponent.js
@@ -1,21 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { SocketContext } from '../../socket.js';
 // pour accéder à ce composant, mettre "import Save from './saveComponent.js';"
 
 function Save({ data }) {
     // Enregistrement de la valeur de la data
     const { socket } = useContext(SocketContext);
-    const party = data.party
+    const [party, setParty] = useState(data.party);
+
   const handleSave = () => {
     console.log("La partie", party, "demande à être mise en pause");
     socket.emit('saveParty', { 'partie': party });
   };
 
-
+  const Style = {
+    position: 'fixed',
+    top: '143px',
+    left: '110px',
+  };
 
   return (
     <div>
-      <button  onClick={handleSave}>Sauvegarder</button>
+      <button style={Style} onClick={handleSave}>Sauvegarder</button>
     </div>
   );
 };
