19b76ca453cb4158c3531bc35b1664a82b1bdf2b
Xerondo <72127675+Xerondo@users.noreply.github.com>
2024-01-26 10:48:15 +0100

    oublié d'enlever un micro truc

--- Projet-Programmation/client/src/Party/SQP.js
+++ Projet-Programmation/client/src/Party/SQP.js
@@ -270,7 +270,6 @@ function quadrillagePoints() {
 
 function Player(props) {
     const img = require('../img/SQP/bonhomme.png');
-    console.log(img);
     const playerContainerStyle = {
         display: 'flex',
         flexDirection: 'column', // Afficher les informations verticalement
