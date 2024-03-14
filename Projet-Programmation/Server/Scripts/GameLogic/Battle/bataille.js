9ea15f5bcc53136cf0d1124b80819c04404f1a70
Le_redstonneur <50110370+LeRedstonneur@users.noreply.github.com>
2024-03-14 14:40:37 +0100

    A finir : comparaison de valeurs de cartes
    L'AS est considéré comme une carte de valeur 1 (donc toujours perdante), à corriger

--- Projet-Programmation/Server/Scripts/GameLogic/Battle/bataille.js
+++ Projet-Programmation/Server/Scripts/GameLogic/Battle/bataille.js
@@ -143,7 +143,12 @@ function suite(io, db, idPartie, nbJoueursPossibles, centre, archive, cartesJoue
 
         let valeurs = Object.keys(compterValeurs); // valeurs comptient la liste des valeurs jouées
         valeurs.sort();
-        let valeurLaPlusGrande = valeurs[valeurs.length - 1];
+        let valeurLaPlusGrande = null;
+        if(valeurs.includes(1)){ // S'il y a un as, c'est la valeur la plus grande
+            valeurLaPlusGrande = 1;
+        } else { // Sinon, la liste étant triée, la dernière valeur est forcément la plus grande
+            valeurLaPlusGrande = valeurs[valeurs.length - 1];
+        }
 
         if (compterValeurs[valeurLaPlusGrande] == '1') { // si la valeur de carte la plus grande est unique
             // On récupère le nom du joueur qui a placé la plus grande carte
