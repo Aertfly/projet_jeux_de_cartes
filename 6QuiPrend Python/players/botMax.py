7aa609d564b537de672862a051616cc58f63ab90
Xerondo <72127675+Xerondo@users.noreply.github.com>
2024-04-22 15:08:27 +0200

    Update botMax.py

--- 6QuiPrend Python/players/botMax.py
+++ 6QuiPrend Python/players/botMax.py
@@ -1,7 +1,7 @@
 from players.player import Player
 from game.card import Card
 
-class BotPlayer(Player):
+class BotMax(botPlayer):
     """
     Le bot qui choisit toujours la valeur la plus grosse
     """
