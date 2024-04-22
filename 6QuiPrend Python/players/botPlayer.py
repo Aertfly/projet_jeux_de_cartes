d20838df5f445f0890695491372ad4af395c2417
Fraizard <94200448+Fraizard@users.noreply.github.com>
2024-04-22 15:15:28 +0200

    choixLigneDefaut botPlayer.py

--- 6QuiPrend Python/players/botPlayer.py
+++ 6QuiPrend Python/players/botPlayer.py
@@ -4,12 +4,33 @@ from game.card import Card
 class BotPlayer(Player):
     def info(self, message):
         """
-        Affiche un message à l'attention du développeur.
+        Affiche un message à l'attention du joueur.
         
         :param message: Le message à afficher.
         """
         print("@"+self.name+" : ",message)
 
+    def getLineToRemove(self, game):
+        """
+        permet d'obtenir la ligne à enlever quand la carte jouée était plus petite, par défaut pour les bots la ligne avec le moins de têtes de boeufs.
+
+        :param game: le jeu en cours
+        :return: la ligne à enlever
+        """
+        line = 0
+        for i in range(3):
+            game.total_cows(table[i]) <= game.total_cows(table[i+1])
+            line = i+1
+        return line
+
+    @abstractmethod
+    def getCardToPlay(self):    
+        """
+        Permet d'obtenir la carte à jouer.
+
+        :return: La réponse du joueur.
+        """    
+        pass
     
     def player_turn(self, game):
         """
