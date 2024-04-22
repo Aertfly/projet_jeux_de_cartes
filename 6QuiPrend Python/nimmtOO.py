316af7597b7bf0da39e0ee3e90b013173f3b4f51
Le_redstonneur <50110370+LeRedstonneur@users.noreply.github.com>
2024-04-22 15:25:44 +0200

    Utilisation bot

--- 6QuiPrend Python/nimmtOO.py
+++ 6QuiPrend Python/nimmtOO.py
@@ -1,16 +1,21 @@
 from players.humanPlayer import HumanPlayer
 from game.nimmtGame import NimmtGame     
+from players.randomBotPlayer import RandomBotPlayer
 
 def interactiveRun():
     print("Bienvenue sur le jeu 6 qui prend !")
     while True:
         try:
-            num_players = int(input("Combien de joueurs ? "))
-            players=[]
-            for i in range(num_players):
-                name=input("Nom du joueur : ")
-                players.append(HumanPlayer(name))
-            game=NimmtGame(players)
+            #num_players = int(input("Combien de joueurs ? "))
+            #players=[]
+            #for i in range(num_players):
+            #    name=input("Nom du joueur : ")
+            #    players.append(HumanPlayer(name))
+            bots = []
+            bots.append(RandomBotPlayer("Aleatoire1"))
+            bots.append(RandomBotPlayer("Aleatoire2"))
+
+            game=NimmtGame(bots)
             scores, winners=game.play()
 
             print("La partie est terminée!")
