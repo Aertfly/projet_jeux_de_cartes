5b453127fbe420ba5eab17c512bcee3fea360408
Aertfly <123474051+Aertfly@users.noreply.github.com>
2024-04-22 14:39:41 +0200

    Update randomBotPlayer.py

--- 6QuiPrend Python/players/randomBotPlayer.py
+++ 6QuiPrend Python/players/randomBotPlayer.py
@@ -1,4 +1,5 @@
 from random import randint 
+from players.botPlayer import botPlayer
 class randomBotPlayer(botPlayer):
     def getCardToPlay(self):
         return self.hand[randint(len(self.hand))]
