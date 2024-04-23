b9c67d843e5e9dce166947dcc7671957a602a8f7
Aertfly <jean.colib.mfc@gmail.com>
2024-04-23 12:38:47 +0200

    Fix randomBot

--- 6QuiPrend Python/players/randomBotPlayer.py
+++ 6QuiPrend Python/players/randomBotPlayer.py
@@ -2,7 +2,7 @@ from random import randint
 from players.botPlayer import BotPlayer
 class RandomBotPlayer(BotPlayer):
     def getCardToPlay(self):
-        return self.hand[randint(0,len(self.hand)-1)].value
+        return self.hand[randint(0,len(self.hand)-1)]
     
     def getLineToRemove(self, game):
         return randint(1,4)
