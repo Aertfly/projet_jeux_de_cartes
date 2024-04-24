d761e969fda38b837c2aaa04c524882349083a9b
Xerondo <72127675+Xerondo@users.noreply.github.com>
2024-04-24 16:21:48 +0200

    BotEchantillon fonctionnel
    La logique marche, faut juste qu'on mette la solution pour que on ait pas besoin de s'envoyer game via player.py mais sinon le bot marche

--- 6QuiPrend Python/players/botEchantillon.py
+++ 6QuiPrend Python/players/botEchantillon.py
@@ -1,6 +1,6 @@
 from players.botPlayer import BotPlayer
 from game.card import Card
-from random import randint
+from random import randint, choice
 
 class BotEchantillon(BotPlayer):
     """
@@ -9,88 +9,62 @@ class BotEchantillon(BotPlayer):
     ramassées.
     """
 
-    def round(carte, table):
+    @staticmethod
+    def roundTest(carte, table):
         pts = 0
+        #print("carte round :", carte.value)
+        #print(table)
+        i = 0
+        while i < len(table) and carte.value > table[i][-1].value:
+            i += 1
 
-        i = 1
-        while carte > table[i][-1]: # Juste pour trouver la ligne où placer la carte
-            i+=1
-        i-=1
-
-        if i == 0:
-            ligne = randin(1, 4)
-            pts = sum(card.cowsNb for card in table[ligne])
-            table[ligne] = [carte]
-        else: 
-            if len(table[i]) == 5 :
-                pts = sum(card.cowsNb for card in table[i])
-                table[i] = [carte]
+        if i == len(table) or carte.value < table[0][-1].value:
+            ligne = randint(1, 4)
+            #print("ligne choisie pour nouvelle carte:", ligne)
+            pts = sum(card.cowsNb for card in table[ligne-1])
+            table[ligne-1] = [carte]
+        else:
+            if len(table[i-1]) == 5:
+                pts = sum(card.cowsNb for card in table[i-1])
+                table[i-1] = [carte]
             else:
-                table[i].append(carte)
+                table[i-1].append(carte)
+
         return pts, table
 
-    def simulation(carteJoueur, autres_cartes_joueurs):
-        plateau_temporaire = [ligne.copy() for ligne in botEchantillon.game.table]
+    @staticmethod
+    def simulation(carteJoueur, autres_cartes_joueurs, game):
+        plateau_temporaire = [ligne.copy() for ligne in game.table]
 
-        for cards in autres_cartes_joueurs:
-            if cards < carteJoueur:
-                sert_a_rien, plateau_temporaire = round(cards, plateau_temporaire)
+        for card_value in autres_cartes_joueurs:
+            card = Card(card_value)
+            if card.value < carteJoueur.value:
+                sert_a_rien, plateau_temporaire = BotEchantillon.roundTest(card, plateau_temporaire)
 
-        nb_points_gagnes, plateau_temporaire = round(carteJoueur, plateau_temporaire)
+        nb_points_gagnes, plateau_temporaire = BotEchantillon.roundTest(carteJoueur, plateau_temporaire)
         return nb_points_gagnes
 
     def getCardToPlay(self, game):
         resultatsRounds = []
 
         listeCartesPossibles = [i+1 for i in range(104)]
-        for element in self.hand:
-            #print("longueur liste :", len(listeCartesPossibles))
-            #print("Element :", (element.value)-1)
-            listeCartesPossibles.remove(element.value)
-        for element in game.alreadyPlayedCards: # à corriger pour faire en sorte que "game.alreadyPlayedCards" soit la liste des cartes déjà joués
-            listeCartesPossibles.remove(element.value)
+        for element in self.hand + game.alreadyPlayedCards:
+            if element.value in listeCartesPossibles:
+                listeCartesPossibles.remove(element.value)
 
-        for _ in range(10):
+        for _ in range(50):
             autreJoueursCartes = []
-            for _ in range(len(game.players)): # à corriger pour faire en sorte que "game.players" soit la liste des noms des joueurs
-                carteRandom = randint(1, len(listeCartesPossibles))
-                while carteRandom in autreJoueursCartes:
-                    carteRandom = randint(1, len(listeCartesPossibles))
-                autreJoueursCartes.append(Card(listeCartesPossibles[carteRandom]))
-            
+            while len(autreJoueursCartes) < len(game.players) - 1:
+                carteRandom = choice(listeCartesPossibles)
+                if carteRandom not in autreJoueursCartes:
+                    autreJoueursCartes.append(carteRandom)
+
             resultatParties = []
             for carte in self.hand:
-                resultatParties.append(botEchantillon.simulation(carte, autreJoueursCartes))
-            [resultatsRounds.append(element) for element in resultatParties if element==0]
+                resultatParties.append(self.simulation(carte, autreJoueursCartes, game))
+            resultatsRounds.extend(resultatParties)
 
-        return max(resultatsRounds, key=resultatsRounds.count)
-      
-"""
-# Exemple de jeu pour tester le botEchantillon
-table_jeu = {
-    1: [10, 23, 25],
-    2: [41, 44],
-    3: [56, 60, 67],
-    4: [80, 81, 83, 91]
-}
+        carte_min_pts = min(resultatsRounds)
+        cartes_min = [carte for carte, pts in zip(self.hand, resultatsRounds) if pts == carte_min_pts]
 
-already_played_cards = [2, 57, 82]  # Cartes déjà jouées
-
-# Création d'une instance de botEchantillon avec des données de jeu
-bot = botEchantillon({
-    'table': table_jeu,
-    'alreadyPlayedCards': already_played_cards,
-    'players': ['Joueur3', 'Joueur2']  # Exemple de noms de joueurs
-})
-
-# Exemple de main du joueur pour tester la méthode getCardToPlay
-hand_joueur = [55, 27, 42, 8]  # Main du joueur
-
-# Assignation de la main du joueur au bot
-bot.hand = hand_joueur
-
-# Test de la méthode getCardToPlay
-carte_a_jouer = bot.getCardToPlay()
-print("Carte à jouer choisie par le bot :", carte_a_jouer)
-"""
-
+        return min(cartes_min, key=lambda c: c.value)  # Retourne la carte avec le moins de points et la plus petite valeur en cas d'égalité
