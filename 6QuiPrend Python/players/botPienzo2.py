30a1bbe358205b0277fad29b35e73da54937f40a
Le_redstonneur <50110370+LeRedstonneur@users.noreply.github.com>
2024-04-26 12:58:20 +0200

    Add files via upload

---
+++ 6QuiPrend Python/players/botPienzo2.py
@@ -0,0 +1,24 @@
+from players.botPlayer import BotPlayer
+from game.card import Card
+
+
+class BotPienzo2(BotPlayer):
+    def player_turn(self, game):
+        safeZone = 3
+        # On détermine la ligne avec le moins de carte
+        minimum, indice = min((len(ligne), i) for i, ligne in enumerate(game.table))
+        # On a trouvé la ligne la plus petite : c'est la ligne numéro "indice", d'une taille de "minimum"
+        while indice < 4:
+            if len(game.table[indice]) == minimum and minimum < safeZone:
+                for carte in self.hand:
+                    if carte > game.table[indice][-1]: # Si on a une carte qui pourrait aller dans la ligne la moins remplie
+                        if indice == 3: # Si on joue sur la dernière ligne
+                            return carte
+                        if carte < game.table[indice+1][-1]: # Si la carte ne pourrait pas aller sur la ligne d'après
+                            return carte
+                        # La carte irait sur la ligne d'après, en fait on ne la joue pas
+            indice+=1
+        return self.hand[-1]
+
+    def getCardToPlay(self):
+        pass
